c423d80f52815bbffb633cce7fe0b952eeb2d2bc
cold5nap <and.andr2013@yandex.ru>
2023-05-21 18:27:22 +0300

    7

--- src/frontend/src/store/index.js
+++ src/frontend/src/store/index.js
@@ -11,7 +11,7 @@ export default createStore({
 	state () {
 		return {
 			// url: 'http://localhost:8080',
-			url: 'http://visual-fuzzy-control-language.fiori-vrn.ru',
+			url: 'http://vfcl.fiori-vrn.ru',
 			isLoading: false,
 			graphic: null,
 			selectedType: null,
